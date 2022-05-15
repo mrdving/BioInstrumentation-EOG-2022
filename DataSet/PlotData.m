fc57d7b2e42bc28b041768399691a2032a07ff5e
CHENG-JU TSAI <chengju.tsai@student.unimelb.edu.au>
2022-05-15 23:44:23 +0000

    DataSet : UPDATE matlab script to extract signal, mirror them, and save to csv files

--- DataSet/PlotData.m
+++ DataSet/PlotData.m
@@ -1,25 +1,93 @@
 %%
 file = readtable("EOG_S1.txt");
 %%
+iSample = 10000;
 nSample = 5000;
-eog = file.EOG(1:nSample);
+eog = file.EOG(iSample:iSample + nSample);
 eog_hp = detrend(eog);
-gaze = file.GAZE_INTERPOLATED(1:nSample);
-time = file.TIME(1:nSample);
+gaze = file.GAZE_INTERPOLATED(iSample:iSample + nSample);
+time = file.TIME(iSample:iSample + nSample);
+
+%%
+eog_fft = fft(eog);
+eog_hp_fft = fft(eog_hp);
+hold on
+plot(linspace(0,83.34, length(time)), 20*log10(abs(eog_fft)))
+plot(linspace(0,83.34, length(time)), 20*log10(abs(eog_hp_fft)))
+hold off
+title("FFT")
+ylabel("PSD(dB)")
+xlabel("frequency(Hz)")
+%xlim([0,50])
+legend("raw", "highpassed")
 %%
 close all
 subplot(3,1,1)
 plot(time, eog)
 title("Raw eog signal")
-xlabel("eog signal(mV)")
-ylabel("time(s)")
+ylabel("eog signal(mV)")
+xlabel("time(s)")
 subplot(3,1,2)
 plot(time, eog_hp)
 title("Eog signal with removed drift")
-xlabel("eog signal(mV)")
-ylabel("time(s)")
+ylabel("eog signal(mV)")
+xlabel("time(s)")
 subplot(3,1,3)
 plot(time, gaze)
 title("Horizontal gaze angle")
-xlabel("gaze angle(deg)")
-ylabel("time(s)")
+ylabel("gaze angle(deg)")
+xlabel("time(s)")
+%% Extract signal
+sig_1 = file.EOG(file.TIME > 10 & file.TIME < 20);
+time_1 = file.TIME(file.TIME > 10 & file.TIME < 20);
+gaze_1 = file.GAZE_INTERPOLATED(file.TIME > 10 & file.TIME < 20);
+
+sig_2 = file.EOG(file.TIME > 120 & file.TIME < 130);
+time_2 = file.TIME(file.TIME > 120 & file.TIME < 130);
+gaze_2 = file.GAZE_INTERPOLATED(file.TIME > 120 & file.TIME < 130);
+
+sig_1 = [sig_1(1:end); sig_1(end:-1:1)];
+time_1 = [time_1(1:end); time_1(1:end) + time_1(end)-time_1(1)];
+gaze_1 = [gaze_1(1:end); gaze_1(end:-1:1)];
+
+sig_2 = [sig_2(1:end); sig_2(end:-1:1)];
+time_2 = [time_2(1:end); time_2(1:end) + time_2(end)-time_2(1)];
+gaze_2 = [gaze_2(1:end); gaze_2(end:-1:1)];
+
+
+writematrix(sig_1, "./testSignal/sig_1.csv")
+writematrix(time_1, "./testSignal/time_1.csv")
+writematrix(gaze_1, "./testSignal/gaze_1.csv")
+
+writematrix(sig_2, "./testSignal/sig_2.csv")
+writematrix(time_2, "./testSignal/time_2.csv")
+writematrix(time_2, "./testSignal/gaze_2.csv")
+
+%%
+subplot(2,1,1)
+plot(time_1, sig_1)
+xlabel("time(s)")
+ylabel("eog signal(mV)")
+subplot(2,1,2)
+plot(time_1, gaze_1)
+ylabel("gaze angle(deg)")
+xlabel("time(s)")
+max(sig_1)
+min(sig_1)
+off1 = 0.5*(max(sig_1) + min(sig_1)) %28.83mV
+amp1 = max(sig_1) - off1 %0.068mV
+
+%%
+subplot(2,1,1)
+plot(time_2, sig_2)
+xlabel("time(s)")
+ylabel("eog signal(mV)")
+subplot(2,1,2)
+plot(time_2, gaze_2)
+ylabel("gaze angle(deg)")
+xlabel("time(s)")
+max(sig_2)
+min(sig_2)
+
+off2 = 0.5*(max(sig_2) + min(sig_2))%26.35mV
+amp2 = max(sig_2) - off2 %0.21mV
